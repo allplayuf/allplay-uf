8a225d67b8b3061466253b4fe357c25c01eab33e
AllPlay <allplayuf@gmail.com>
2025-11-24 09:55:07 +0000

    File changes

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,6 +5,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Badge } from "@/components/ui/badge";
+import { createPageUrl } from "@/components/utils/helpers";
 import {
   MessageSquare,
   TrendingUp,
